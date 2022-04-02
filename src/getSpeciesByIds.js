7aa42f85aaba11a41f395d06f99980d3dc919617
RamonaS2 <bandeiraramona2@gmail.com>
2022-04-02 12:44:19 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,9 @@
-/* eslint-disable global-require */
 const data = require('../data/zoo_data');
-// se a especie index contiver o id passsado como parametro eu vou retornar uma array com esse;
-// toContain()
-
-// console.log(Object.entries(species[0]));
-// const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const { species } = require('../data/zoo_data');
-  const animaisFiltrados = species.filter((animal) => ids.find((id) => id === animal.id));
+  const { species } = data;
+  const animaisFiltrados = species.filter((animal) =>
+    ids.find((id) => id === animal.id));
 
   return animaisFiltrados;
 }
