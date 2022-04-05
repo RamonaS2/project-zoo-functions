ab324b27b4a5ce87ccf8c69c28dfc1ba6655f2d5
RamonaS2 <bandeiraramona2@gmail.com>
2022-04-04 23:30:25 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
+// se o manager > manager.length então é um gerente
 
 function isManager(id) {
-  // seu código aqui
+  const gerente = employees.some((elemento) => elemento.managers.includes(id));
+
+  return gerente;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const responsavel = employees.filter((fun) => fun.managers.includes(managerId));
+    const pupilos = responsavel.map((ele) => `${ele.firstName} ${ele.lastName}`);
+    return pupilos;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
