81e38b52d34dd9dfdf23cb75abf331e1cef7b82f
RamonaS2 <bandeiraramona2@gmail.com>
2022-04-05 20:35:17 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((fun) => fun.id === id);
+  const animais = species.find((animal) => animal.id === funcionario.responsibleFor[0]);
+  const animalMais = animais.residents.sort((x, y) => y.age - x.age)[0];
+
+  return Object.values(animalMais);
 }
 
 module.exports = getOldestFromFirstSpecies;
