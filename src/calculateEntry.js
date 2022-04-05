1a66b42f9c5ab2ba856fea751c6615c08519c964
RamonaS2 <bandeiraramona2@gmail.com>
2022-04-05 19:20:53 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const menosDezoito = entrants.filter((criança) => criança.age < 18);
+  const crianças = menosDezoito.length;
+  const maisDezoito = entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50);
+  const adultos = maisDezoito.length;
+  const maisCinquenta = entrants.filter((idoso) => idoso.age >= 50);
+  const idosos = maisCinquenta.length;
+  return { child: crianças, adult: adultos, senior: idosos };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const pessoas = countEntrants(entrants);
+  const entrantsAdult = pessoas.adult * prices.adult;
+  const entrantsChild = pessoas.child * prices.child;
+  const entrantsSenior = pessoas.senior * prices.senior;
+  return entrantsAdult + entrantsChild + entrantsSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
